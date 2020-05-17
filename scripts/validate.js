781daaf7961a6ed4ffe1a18d88d2d5e81823b5b8
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-05-17 14:07:58 +0300

    feat: реализовать валидацию формы редактирования профиля ver.6

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,16 +1,31 @@
+//VARIABLES
+//form popups validation vars
+const elementProfile = document.querySelector('.profile');
+
+
 //FUNCTIONS
 //form popups validation fuctions
-function showInputError(element) {
-  element.classList.add('popup__input-text_type_error');
+function showInputError(elementInput, errorMessage) {
+  const elementInputError = document.querySelector(`#${elementInput.id}-error`);
+
+  elementInput.classList.add('popup__input-text_type_error');
+  elementInputError.classList.add('popup__input-error_show');
+
+  elementInputError.textContent = errorMessage; //*передали спэну в качестве текста сообщение об ошибке из параметра
 };
 
-function hideInputError(element) {
-  element.classList.remove('popup__input-text_type_error');
+function hideInputError(elementInput) {
+  const elementInputError = document.querySelector(`#${elementInput.id}-error`);
+
+  elementInput.classList.remove('popup__input-text_type_error');
+  elementInputError.classList.remove('popup__input-error_show');
+
+  elementInputError.textContent = '';
 };
 
 function isValid(evt) {
-  if (!evt.target.validity.valid) {
-    showInputError(evt.target);
+  if (!evt.target.validity.valid) { //*если инпут не проходит валидацию - предупреждать об ошибке
+    showInputError(evt.target, evt.target.validationMessage); //*вторым аргументом передали стоковое сообщение браузера об ошибке валидации
   } else {
     hideInputError(evt.target);
   }
@@ -18,5 +33,4 @@ function isValid(evt) {
 
 //LISTENERS
 //form popups validation listeners
-document.addEventListener('input', isValid);
-
+elementProfile.addEventListener('input', isValid); //*отслеживаем все дочерние инпуты элемента profile, за счёт делегирования
