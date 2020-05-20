834e467befab5e0f0fe5c61a24fdabff0080825f
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-05-20 10:17:16 +0300

    feat: реализовать валидацию формы редактирования профиля ver.24

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,3 +1,12 @@
+const enableValidationArgs = {
+  formSelector: '.popup__container',
+  inputSelector: '.popup__input-text',
+  submitButtonSelector: '.popup__submit',
+  invalidButtonClass: 'popup__submit_invalid',
+  inputErrorClass: 'popup__input-text_type_error',
+  errorClass: 'popup__input-error_show'
+}
+
 //FUNCTIONS
 //form popups validation fuctions
 function showInputError(formElement, inputElement, errorMessage) {
