f0419a84e83deb121bb70d878257bde21aed750b
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-06-20 17:57:28 +0300

    refactor: реализовать ООП ver.28

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -6,24 +6,20 @@ export class Popup {
     this._popup = document.querySelector(popupSelector);
   }
 
-  open() {
-    this._popup.classList.add('popup_opened');
-
-    document.addEventListener('keydown', (evt) => {
-      this._handleEscClose(evt);
-    }); //*todo надо этот лисенер добавлять при открытии попапа и сносить при закрытии
-        //todo надо как то создасть именованную функцию-обработчик, чтобы была возможность снести лисенер (через функциональное выражение его снести не получится)
-  }
-
-  close() {
-    this._popup.classList.remove('popup_opened');
-    // document.removeEventListener('keydown', escapeAddPopup); //todo если именованная функция как в этом примере, то снесет лисенер, придумаеть как это сделать
-  }
-
   _handleEscClose(evt) {
     if (evt.key === 'Escape') {
       this.close();
     }
+  }
+
+  open() {
+    this._popup.classList.add('popup_opened');
+    document.addEventListener('keydown', this._handleEscClose);
+  }
+
+  close() {
+    this._popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 
   setEventListeners() {
