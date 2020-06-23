61b296a340cf94437fa59985cd38f969b826f160
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-06-23 12:11:58 +0300

    refactor: исправить ошибки после КР ver.2

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,12 @@
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-//подключили плагины в файл
+//*подключили плагины в файл
 
 module.exports = {
   plugins: [
-    autoprefixer, //подключили autoprefixer
+    autoprefixer, //*подключили autoprefixer
     cssnano({ preset: 'default' })
-    //cssnano при подключении передали объект опций
-    //{ preset: default } говорит о том, что нужно использовать стандартные настройки минификации
-  ] //подключили плагины к PostCSS
+    //*cssnano при подключении передали объект опций
+    //*{ preset: default } говорит о том, что нужно использовать стандартные настройки минификации
+  ] //*подключили плагины к PostCSS
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,32 @@
-const path = require('path'); //подключили метод path к конфигу webpack
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //подключили плагин для работы с HTML
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //подключили плагин для объединения БЭМ css-файлов в один
+const path = require('path'); //*подключили метод path к конфигу webpack
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //*подключили плагин для работы с HTML
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //*подключили плагин для объединения БЭМ css-файлов в один
 
 module.exports = {
-  entry: { main: './src/pages/index.js' }, //указали точку входа - первое место куда заглянет webpack при сборке
+  entry: { main: './src/pages/index.js' }, //*указали точку входа - первое место куда заглянет webpack при сборке
   output: {
-    path: path.resolve(__dirname, 'dist'), //переписали точку выхода - итоговый файл, куда webpack сложит весь js-код (использовали утилиту path)
+    path: path.resolve(__dirname, 'dist'), //*переписали точку выхода - итоговый файл, куда webpack сложит весь js-код (использовали утилиту path)
     filename: 'main.js'
   },
   module: {
-    rules: [ //rules — это массив правил
+    rules: [ //*rules — это массив правил
       {
-        test: /\.js$/, //регулярное выражение, которое ищет все js файлы
-        loader: 'babel-loader', //при обработке этих файлов нужно использовать babel-loader
-        exclude: '/node_modules/' //исключает папку node_modules, файлы в ней обрабатывать не нужно
-      }, //добавили объект правил для бабеля
+        test: /\.js$/, //*регулярное выражение, которое ищет все js файлы
+        loader: 'babel-loader', //*при обработке этих файлов нужно использовать babel-loader
+        exclude: '/node_modules/' //*исключает папку node_modules, файлы в ней обрабатывать не нужно
+      }, //*добавили объект правил для бабеля
       {
-        test: /\.(png|svg|jpg|gif|woff|woff2)$/, //регулярное выражение, которое ищет все файлы с такими расширениями
-        loader: 'file-loader'
-      }, //добавили правило для добавления файлов в сборку
+        test: /\.(png|svg|jpg|gif)$/, //*регулярное выражение, которое ищет все файлы с такими расширениями
+        loader: 'file-loader?name=./images/[name].[ext]' //*прописали, чтобы файлы изображений собирались в отдельную папку
+      }, //*добавили правило для добавления файлов в сборку
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/[name].[ext]', //*прописали, чтобы файлы шрифтов собирались в отдельную папку
+      },
       {
         test: /\.html$/,
         loader: 'html-loader'
-      }, //добавили правило, чтобы подставить правильные пути файлов в HTML
+      }, //*добавили правило, чтобы подставить правильные пути файлов в HTML
       {
         test: /\.css$/,
         loader: [
@@ -33,13 +37,13 @@ module.exports = {
           },
           'postcss-loader'
         ]
-      } //добавили правило для обработки css-файлов
+      } //*добавили правило для обработки css-файлов
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html' //прописали путь к файлу index.html
+      template: './src/index.html' //*прописали путь к файлу index.html
     }),
-    new MiniCssExtractPlugin() //подключили плагин для объединения css-файлов
+    new MiniCssExtractPlugin() //*подключили плагин для объединения css-файлов
   ]
 };
