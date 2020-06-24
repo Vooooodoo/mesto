1e3fbef021c7c8de06f639516227206d4b7da3de
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-06-24 12:05:55 +0300

    refactor: исправить ошибки после КР

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -6,15 +6,15 @@ import { Popup } from './Popup.js';
 export class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+    this._photoPopupPhoto = this._popup.querySelector('.popup__photo');
+    this._photoPopupTitle = this._popup.querySelector('.popup__photo-title');
+    //*чтобы поиск данных элементов не выполнялся при каждом вызове метода open, объявили их в конструкторе класса, а не в теле метода
   }
 
   open(name, link) { //*добавили два параметра, аргументы получим из класса Card, при вызове этого метода в index.js, как тела колбэк-функции handleCardClick
-    const photoPopupPhoto = this._popup.querySelector('.popup__photo');
-    const photoPopupTitle = this._popup.querySelector('.popup__photo-title');
-
-    photoPopupPhoto.src = link;
-    photoPopupPhoto.alt = `${name}.`;
-    photoPopupTitle.textContent = name;
+    this._photoPopupPhoto.src = link;
+    this._photoPopupPhoto.alt = `${name}.`;
+    this._photoPopupTitle.textContent = name;
 
     super.open();
   }
